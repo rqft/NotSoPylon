5fc8100b79e4eb60fd61dfab243aeab2d3e07dfc
Arcy <a@rqft.space>
2021-08-24 15:20:21 -0500

    a

--- src/internals/client.ts
+++ src/internals/client.ts
@@ -2,7 +2,34 @@ export class NotSoClient {
   private _commandGroup: discord.command.CommandGroup;
   constructor(commands: NotSoCommand[]) {
     this._commandGroup = new discord.command.CommandGroup();
-    commands.forEach((v) => {});
+    commands.forEach((v) => {
+      this._commandGroup.on({
+        name: (v.prefix || "") + v.name,
+        description: v.description ?? "good luck",
+        aliases: v.aliases ?? [],
+      });
+    });
   }
 }
-export interface NotSoCommand {}
+export interface NotSoCommand<
+  Args extends { [key: string]: discord.command.CommandArgumentTypes } = {}
+> {
+  args?: IArgument;
+  name: string;
+  aliases?: Array<string>;
+  prefix?: string;
+  description?: string;
+  run: (
+    message: discord.GuildMemberMessage,
+    args: discord.command.ResolvedArgs<Args>
+  ) => any;
+}
+export interface IArgument<T> {
+  name: string;
+  type: discord.command.ArgumentType;
+  description?: string;
+  default?: T;
+  choices?: string[] | number[];
+  minValue?: number;
+  maxValue?: number;
+}
